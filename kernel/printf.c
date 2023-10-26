a23ba34820998ef8e0db7b313221dd78f049962d
Andrej Seres <xseres@stuba.sk>
2023-10-26 12:41:25 +0200

    feat: dajaky pokus o backtrace ig

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -132,4 +133,20 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  uint64 limit_down = PGROUNDDOWN(fp);
+  uint64 limit_up = PGROUNDUP(fp);
+  printf("backtrace:\n");
+
+  while(fp >= limit_down && fp < limit_up) {
+    printf("%p\n", *((uint64*)(fp - 8)));
+    fp = *((uint64*)(fp - 16));
+  }
 }
